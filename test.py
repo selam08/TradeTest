ae069bca223d94dc583f6cf45cedd6ece5f8ead0
selam08 <selim.amrari@epfl.ch>
2018-05-05 01:05:35 +1100

    read specific ticket or all ticket from capm. into dataframe.

--- test.py
+++ test.py
@@ -1,19 +1,29 @@
 import urllib.request
 import json
+import pandas as pd
 
-def getCoins():
-        coin = open("coins.txt", "r").read()
-        coins = coin.split("\n")
-        for c in coins:
-                if(c != ""):
-                  print(c + ": " + getRate(c))
+'''def getCoins():
+    coin = open("coins.txt", "r").read()
+    coins = coin.split("\n")
+    for c in coins:
+            if(c != ""):
+              print(c + ": " + getRate(c))'''
 
 
 def getRate(coin):
-        r = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/" + coin)
-        d = json.load(r)
-        data = d[0]
-        return(data['price_usd'])
+    r = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/" + coin)
+    d = json.load(r)
+    data = d[0]
+    return(data['price_usd'])
 
-getCoins()
- 
+def getTicket(coin):
+    r = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/" + coin)
+    data = json.load(r)
+    return data
+
+def getTicket2Df(coin=''):
+    data = pd.read_json("https://api.coinmarketcap.com/v1/ticker/" + coin)
+    return data
+
+dataBTC = getTicket2Df()
+print(dataBTC)
