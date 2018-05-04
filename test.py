4a87936f7adfb484fe47f3962a79a6ebe1300cb2
selam08 <selim.amrari@epfl.ch>
2018-05-05 00:11:59 +1100

    let's begin: get info

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-import numpy as np
+import urllib.request
+import json
 
-a = np.arrange(5)
-print(a)
+def getCoins():
+        coin = open("coins.txt", "r").read()
+        coins = coin.split("\n")
+        for c in coins:
+                if(c != ""):
+                  print(c + ": " + getRate(c))
+
+
+def getRate(coin):
+        r = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/" + coin)
+        d = json.load(r)
+        data = d[0]
+        return(data['price_usd'])
+
+getCoins()
